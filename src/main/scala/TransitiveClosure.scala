c4fdd11962c3ba9c17f79224e108511d621ea70d
VladimirLogachev <doit@keemail.me>
2019-10-08 02:42:57 +0700

    cleanup and optimize readClosure function

--- src/main/scala/TransitiveClosure.scala
+++ src/main/scala/TransitiveClosure.scala
@@ -17,23 +17,23 @@ trait FooRepository[F[_]] {
 
 object TransitiveClosure extends App {
   def readClosure[F[_] : Monad](repo: FooRepository[F], ids: List[FooId]): F[Set[Foo]] = {
+    type LoopState = (Set[FooId], Set[FooId], Set[Foo])
 
-    def loop(existingItems: Set[Foo]): F[Either[Set[Foo], Set[Foo]]] =
-      existingItems
-        .flatMap(_.references)
-        .removedAll(existingItems.map(_.id)) // prevent from fetching existing items again
-        .toList match {
-        case List() => Applicative[F].pure(Right(existingItems)) // prevent from passing an empty list of ids
-        case ids =>
-          repo
-            .read(ids)
-            .flatMap(newItems =>
-              if (newItems.isEmpty) Applicative[F].pure(Right(existingItems)) // exit tailrec
-              else Applicative[F].pure(Left(existingItems ++ newItems))) // continue tailrec
-      }
+    def loop(loopState: LoopState): F[Either[LoopState, Set[Foo]]] = {
+      val (alreadyFetchedIds, idsToFetch, allItems) = loopState
+      repo.read(idsToFetch.toList).flatMap(newItems => {
+        val idsToSkip = alreadyFetchedIds ++ newItems.map(_.id)
+        val remainingRefs = newItems.flatMap(_.references).removedAll(idsToSkip) // prevent from fetching existing items again
+        val updatedItems = allItems ++ newItems
+        if (remainingRefs.isEmpty) Applicative[F].pure(Right(updatedItems)) // exit recursion in 2 cases: items returned, no refs left AND no items returned (which is an illegal state, but anyways...)
+        else Applicative[F].pure(Left((idsToSkip, remainingRefs, updatedItems))) // continue recursion
+      })
+    }
 
-    repo
-      .read(ids)
-      .flatMap(Monad[F].tailRecM(_)(loop))
+    repo.read(ids).flatMap(results => {
+      val idsToSkip = results.map(_.id)
+      val notFetchedIds = results.flatMap(_.references).removedAll(idsToSkip)
+      Monad[F].tailRecM((idsToSkip, notFetchedIds, results))(loop)
+    })
   }
 }
